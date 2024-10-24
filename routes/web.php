8c0c5f4afb830623058ae177fa183332575d7b6a
AgungPajar <presetpjr@gmail.com>
2024-10-24 11:34:30 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\DepartemenController;
+use App\Models\Departemen;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+route::resource('departemen',DepartemenController::class);
